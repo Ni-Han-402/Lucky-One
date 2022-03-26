82b0ffc632a2107c5ff5142fd3dbfff5d3a7daee
Nihan Zyan <nihanzyan38@gmail.com>
2022-03-26 15:54:44 +0600

    product Handler Added

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Product.css'
 
-const Product = ({product}) => {
+const Product = ({product, handleAddToCart}) => {
     const {img, name, price} = product;
+    
     
     return (
         <div className='product'>
             <img src={img} alt=""/>
             <h4>{name}</h4>
             <p>Price: {price}Tk</p>
-            <button className='cart-btn'><p>Add to Cart</p></button>
+            <button onClick={() => handleAddToCart(product)} className='cart-btn'><p>Add to Cart</p></button>
         </div>
     );
 };
