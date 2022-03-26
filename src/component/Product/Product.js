ef8e16fa2b205e31964dedd396e3cfbf46deb34e
Nihan Zyan <nihanzyan38@gmail.com>
2022-03-26 22:41:10 +0600

    Font Awesome Added

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Product.css'
 
@@ -10,7 +12,10 @@ const Product = ({product, handleAddToCart}) => {
             <img src={img} alt=""/>
             <h4>{name}</h4>
             <p>Price: {price}Tk</p>
-            <button onClick={() => handleAddToCart(product)} className='cart-btn'><p>Add to Cart</p></button>
+            <button onClick={() => handleAddToCart(product)} className='cart-btn'>
+                <p>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+                </button>
         </div>
     );
 };
