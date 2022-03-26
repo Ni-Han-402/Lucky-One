12401139268a55426d829991ca9bba5407dd85b1
Nihan Zyan <nihanzyan38@gmail.com>
2022-03-26 20:39:29 +0600

    Selected One Added

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([]);
+    const [random, setRandom] = useState([])
+
+
+
 
     useEffect( () =>{
         fetch('products.json')
@@ -17,7 +20,17 @@ const Shop = () => {
         const newCart = [...cart, product];
         setCart(newCart);
     }
+
+    const chooseOneItem = () => {
+           const random = [cart[Math.floor(Math.random()*cart.length)]];
+            setRandom(random);
+            console.log(random);
+    }
+
+   
+
     return (
+        
         <div className='shop-container'>
             <div className="product-container">
                 {
@@ -30,7 +43,39 @@ const Shop = () => {
                  
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+            <div className='cart-section'>
+                <h2>Selected Items</h2>
+                {
+                    cart.map(item => <div 
+                        key={item.id} 
+                        >
+                            
+                            <div className="cart">
+                                <img key={item.id} src={item.img} alt="" />
+                                <p>{item.name}</p>
+                            </div>
+                        
+                        </div>)       
+                }
+                {
+                    
+                        random.map(item => 
+                            <div key={item.id}>
+                            <div className="select-item">
+                                <img src={item.img} alt="" />
+                                <p>{item.name}</p>
+                            </div>
+                            </div>)
+                    
+                }
+                <div>
+                    <button onClick={chooseOneItem} ><p>choose One</p></button>
+                    {/* <button onClick={removeCart} ><p>Remove</p></button> */}
+                </div>
+            </div>
+
+                
+               
             </div>
         </div>
     );
